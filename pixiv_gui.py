b492aa44fe2ccb797c540c2534b7566d8079cf16
B-R-D <xingxuanma@gmail.com>
2019-04-20 12:39:33 +0900

    Ver.0.3.6 Developing GUI Optimize table view

--- pixiv_gui.py
+++ pixiv_gui.py
@@ -5,8 +5,10 @@ from functools import partial
 
 import requests
 from PyQt5.QtCore import Qt, QSettings, QThread, pyqtSignal, QVariant
-from PyQt5.QtWidgets import QVBoxLayout, QFormLayout, QGridLayout, QHeaderView, QTableWidgetItem
-from PyQt5.QtWidgets import QWidget, QGroupBox, QLineEdit, QPushButton, QCheckBox, QMessageBox, QTableWidget, QLabel
+from PyQt5.QtWidgets import (QVBoxLayout, QHBoxLayout, QFormLayout, QGridLayout, QHeaderView, QTableWidgetItem,
+                             QSplitter, QButtonGroup, QWidget, QGroupBox, QLineEdit, QPushButton, QCheckBox,
+                             QMessageBox, QTableWidget, QLabel, QAbstractItemView)
+from PyQt5.QtGui import QBrush, QColor
 
 import globj
 import pixiv
@@ -27,10 +29,11 @@ class LoginWidget(QWidget):
         self.button_ok = QPushButton('登陆')
         self.button_ok.clicked.connect(self.login)
         self.login_thread = None
+        self.verify_thread = None
 
         self.init_ui()
 
-    def _set_disabled(self, status: bool):
+    def set_disabled(self, status: bool):
         self.ledit_pw.setDisabled(status)
         self.ledit_un.setDisabled(status)
         self.cbox_cookie.setDisabled(status)
@@ -66,7 +69,7 @@ class LoginWidget(QWidget):
         If cookies in setting is not NULL, test it by fetching following.
         Or login by username and password.
         """
-        self._set_disabled(True)
+        self.set_disabled(True)
         password = self.ledit_pw.text()
         username = self.ledit_un.text()
         proxy = self.glovar.proxy
@@ -76,21 +79,17 @@ class LoginWidget(QWidget):
         self.settings.endGroup()
         if cookies and not password and not username:
             self.glovar.session.cookies.update(cookies)
-            try:
-                pixiv.get_following(self.glovar.session, proxy)  # Cookies test
-            except (ConnectionError, requests.Timeout):
-                globj.show_messagebox(self, QMessageBox.Warning, '连接失败', '请检查网络或使用代理。')
-            except globj.ResponseError:
-                globj.show_messagebox(self, QMessageBox.Critical, '登陆失败', '请尝试清除cookies重新登陆。')
-            else:
-                self.set_cookies()
-            self._set_disabled(False)
+            self.verify_thread = VerifyThread(self.glovar.session, self.glovar.proxy)
+            self.verify_thread.verify_success.connect(self.set_cookies)
+            self.verify_thread.except_signal.connect(globj.show_messagebox)
+            self.verify_thread.finished.connect(partial(self.set_disabled, False))
+            self.verify_thread.start()
         else:
             self.login_thread = LoginThread(self.glovar.session, proxy, password, username)
-            self.login_thread.start()
             self.login_thread.login_success.connect(self.set_cookies)
             self.login_thread.except_signal.connect(globj.show_messagebox)
-            self.login_thread.finished.connect(partial(self._set_disabled, False))
+            self.login_thread.finished.connect(partial(self.set_disabled, False))
+            self.login_thread.start()
 
     def set_cookies(self):
         self.settings.beginGroup('Cookies')
@@ -101,7 +100,7 @@ class LoginWidget(QWidget):
         self.settings.sync()
         self.settings.endGroup()
         self.login_success.emit()
-        self._set_disabled(False)
+        self.set_disabled(False)
 
 
 class LoginThread(QThread):
@@ -118,8 +117,10 @@ class LoginThread(QThread):
     def run(self):
         try:
             pixiv.login(self.session, proxy=self.proxy, pw=self.pw, uid=self.uid)
-        except (ConnectionError, requests.Timeout):
-            self.except_signal.emit(self.parent(), QMessageBox.Warning, '连接失败', '请检查网络或使用代理。')
+        except (requests.exceptions.ProxyError,
+                requests.exceptions.Timeout,
+                requests.exceptions.ConnectionError) as e:
+            self.except_signal.emit(self.parent(), QMessageBox.Warning, '连接失败', '请检查网络或使用代理。\n' + repr(e))
         except globj.ValidationError:
             self.except_signal.emit(self.parent(), QMessageBox.Critical, '错误', '登陆名或密码错误。')
         except globj.ResponseError as e:
@@ -129,63 +130,220 @@ class LoginThread(QThread):
             self.login_success.emit()
 
 
+class VerifyThread(QThread):
+    verify_success = pyqtSignal()
+    except_signal = pyqtSignal(object, int, str, str)
+
+    def __init__(self, session, proxy):
+        super().__init__()
+        self.session = session
+        self.proxy = proxy
+
+    def run(self):
+        try:
+            pixiv.get_following(self.session, self.proxy)
+        except (requests.exceptions.ProxyError,
+                requests.exceptions.Timeout,
+                requests.exceptions.ConnectionError) as e:
+            self.except_signal.emit(self.parent(), QMessageBox.Warning, '连接失败', '请检查网络或使用代理。\n' + repr(e))
+        except globj.ResponseError:
+            self.except_signal.emit(self.parent(), QMessageBox.Critical, '登陆失败', '请尝试清除cookies重新登陆。')
+        else:
+            self.verify_success.emit()
+
+
+class FetchThread(QThread):
+    fetch_success = pyqtSignal(list)
+    except_signal = pyqtSignal(object, int, str, str)
+
+    def __init__(self, session, proxy, pid, uid, num):
+        super().__init__()
+        self.session = session
+        self.proxy = proxy
+        self.pid = pid
+        self.uid = uid
+        self.num = num
+
+    def run(self):
+        try:
+            if self.pid:
+                new_set = {self.pid}
+            else:
+                new_set = pixiv.get_new(self.session, self.proxy, user_id=self.uid, num=self.num)
+            updater = []
+            results = []
+            for pid in new_set:
+                fet_pic = pixiv.fetcher(pid)
+                if not fet_pic:
+                    print('Not in database.')
+                    fet_pic = pixiv.get_detail(self.session, pid=pid, proxy=self.proxy)
+                    updater.append(fet_pic)
+                else:
+                    print('Fetch from database')
+                results.append(fet_pic)
+            pixiv.pusher(updater)
+        except (requests.exceptions.ProxyError,
+                requests.exceptions.Timeout,
+                requests.exceptions.ConnectionError) as e:
+            self.except_signal.emit(self.parent(), QMessageBox.Warning, '连接失败', '请检查网络或使用代理。\n' + repr(e))
+        except globj.ResponseError as e:
+            self.except_signal.emit(self.parent(), QMessageBox.Critical,
+                                    '未知错误', '返回值错误，请向开发者反馈\n{0}'.format(repr(e)))
+        else:
+            self.fetch_success.emit(results)
+
+
 class MainWidget(QWidget):
     def __init__(self, glovar):
         super().__init__()
         self.glovar = glovar
         self.settings = QSettings(os.path.join(os.path.abspath('.'), 'settings.ini'), QSettings.IniFormat)
+        self.fetch_thread = None
 
         self.ledit_pid = QLineEdit()
         self.ledit_uid = QLineEdit()
         self.ledit_num = QLineEdit()
+
+        self.button_fo = QPushButton('关注的更新')
+        self.button_fo.setCheckable(True)
+        self.button_pid = QPushButton('按PID搜索')
+        self.button_pid.setCheckable(True)
+        self.button_uid = QPushButton('按UID搜索')
+        self.button_uid.setCheckable(True)
+        self.button_loc = QPushButton('本地搜索')
+        self.button_loc.setCheckable(True)
+        self.button_group = QButtonGroup()
+        self.button_group.addButton(self.button_fo, 1)
+        self.button_group.addButton(self.button_pid, 2)
+        self.button_group.addButton(self.button_uid, 3)
+        self.button_group.addButton(self.button_loc, 4)
+        self.button_group.buttonClicked[int].connect(self.change_stat)
+
         self.button_get = QPushButton('获取信息')
-        self.button_get.clicked.connect(self.fetch_info)
-        self.button_fo = QPushButton('关注的新作品')
-        self.button_fo.clicked.connect(self.fetch_new)
+        self.button_get.clicked.connect(self.fetch_new)
         self.button_dl = QPushButton('下载')
+        self.button_logout = QPushButton('退出登陆')
 
         self.table_viewer = QTableWidget()  # Detail viewer of fetched info
+        self.table_viewer.setWordWrap(False)
+        self.table_viewer.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.table_viewer.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.table_viewer.setColumnCount(6)
-        self.table_viewer.setSortingEnabled(True)
         self.table_viewer.verticalHeader().setSectionResizeMode(QHeaderView.Fixed)
-        self.table_viewer.setHorizontalHeaderLabels(['PID', '画廊名', '画师ID', '画师名', '创建日期', '页数'])
+        self.table_viewer.setHorizontalHeaderLabels(['PID', '画廊名', '画师ID', '画师名', '页数', '创建日期'])
         self.table_viewer.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
-        # self.file_table.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
+        self.table_viewer.horizontalHeader().setSectionResizeMode(1, QHeaderView.Stretch)
+        self.table_viewer.horizontalHeader().setSectionResizeMode(3, QHeaderView.Stretch)
+        self.table_viewer.horizontalHeader().setStyleSheet('QHeaderView::section{background-color:#66CCFF;}')
+        self.table_viewer.horizontalHeader().setHighlightSections(False)
 
+        self.ledit_pid.setDisabled(True)
+        self.ledit_num.setDisabled(True)
+        self.ledit_uid.setDisabled(True)
         self.init_ui()
 
     def init_ui(self):
-        controller = QGridLayout()
-        controller.addWidget(QLabel('画廊ID'), 0, 0, 1, 1)
-        controller.addWidget(self.ledit_pid, 0, 1, 1, 1)
-        controller.addWidget(QLabel('用户ID'), 1, 0, 1, 1)
-        controller.addWidget(self.ledit_uid, 1, 1, 1, 1)
-        controller.addWidget(QLabel('数量'), 2, 0, 1, 1)
-        controller.addWidget(self.ledit_num, 2, 1, 1, 1)
-        controller.addWidget(self.button_get, 0, 6, 1, 1)
-        controller.addWidget(self.button_fo, 1, 6, 1, 1)
-        controller.addWidget(self.button_dl, 2, 6, 1, 1)
+        glay_lup = QHBoxLayout()
+        glay_lup.addWidget(self.button_fo)
+        glay_lup.addWidget(self.button_pid)
+        glay_lup.addWidget(self.button_uid)
+        glay_lup.addWidget(self.button_loc)
+
+        glay_ldown = QGridLayout()
+        glay_ldown.addWidget(QLabel('画廊ID'), 0, 0, 1, 1)
+        glay_ldown.addWidget(self.ledit_pid, 0, 1, 1, 5)
+        glay_ldown.addWidget(QLabel('用户ID'), 1, 0, 1, 1)
+        glay_ldown.addWidget(self.ledit_uid, 1, 1, 1, 5)
+        glay_ldown.addWidget(QLabel('数量'), 2, 0, 1, 1)
+        glay_ldown.addWidget(self.ledit_num, 2, 1, 1, 1)
+        glay_ldown.setColumnStretch(1, 1)
+        glay_ldown.setColumnStretch(2, 5)
+
+        vlay_left = QVBoxLayout()
+        vlay_left.addLayout(glay_lup)
+        vlay_left.addLayout(glay_ldown)
+        left_wid = QWidget()
+        left_wid.setLayout(vlay_left)
+
+        glay_right = QGridLayout()
+        glay_right.addWidget(self.button_logout, 0, 0, 1, 1)
+        glay_right.addWidget(self.button_get, 2, 0, 1, 1)
+        glay_right.addWidget(self.button_dl, 3, 0, 1, 1)
+        right_wid = QWidget()
+        right_wid.setLayout(glay_right)
+
+        splitter = QSplitter(Qt.Horizontal)
+        splitter.setHandleWidth(3)
+        splitter.addWidget(left_wid)
+        splitter.addWidget(right_wid)
+        splitter.handle(1).setDisabled(True)
 
         vlay_main = QVBoxLayout()
-        vlay_main.addLayout(controller)
+        vlay_main.addWidget(splitter)
         vlay_main.addWidget(self.table_viewer)
         self.setLayout(vlay_main)
 
-    def _set_disabled(self, status: bool):
-        self.ledit_pid.setDisabled(status)
-        self.ledit_uid.setDisabled(status)
-        self.ledit_num.setDisabled(status)
+    def set_disabled(self, status: bool):
         self.button_get.setDisabled(status)
-        self.button_fo.setDisabled(status)
         self.button_dl.setDisabled(status)
+        self.button_logout.setDisabled(status)
+
+    def change_stat(self, bid):
+        func = {1: self.new_stat,
+                2: self.pid_stat,
+                3: self.uid_stat,
+                4: self.loc_stat}
+        self.ledit_num.clear()
+        self.ledit_pid.clear()
+        self.ledit_uid.clear()
+        func[bid]()
+
+    def new_stat(self):
+        self.button_uid.setChecked(False)
+        self.button_pid.setChecked(False)
+        self.button_loc.setChecked(False)
+        self.button_fo.setChecked(True)
+        self.ledit_uid.setDisabled(True)
+        self.ledit_pid.setDisabled(True)
+        self.ledit_num.setDisabled(False)
+
+    def pid_stat(self):
+        self.button_fo.setChecked(False)
+        self.button_loc.setChecked(False)
+        self.button_uid.setChecked(False)
+        self.button_pid.setChecked(True)
+        self.ledit_uid.setDisabled(True)
+        self.ledit_num.setDisabled(True)
+        self.ledit_pid.setDisabled(False)
+
+    def uid_stat(self):
+        self.button_fo.setChecked(False)
+        self.button_loc.setChecked(False)
+        self.button_pid.setChecked(False)
+        self.button_uid.setChecked(True)
+        self.ledit_pid.setDisabled(True)
+        self.ledit_uid.setDisabled(False)
+        self.ledit_num.setDisabled(False)
+
+    def loc_stat(self):
+        self.button_pid.setChecked(False)
+        self.button_uid.setChecked(False)
+        self.button_fo.setChecked(False)
+        self.button_loc.setChecked(True)
+        self.ledit_uid.setDisabled(False)
+        self.ledit_pid.setDisabled(False)
+        self.ledit_num.setDisabled(False)
 
     def tabulate(self, items):
+        self.table_viewer.setSortingEnabled(False)
+        self.table_viewer.clearContents()
         self.table_viewer.setRowCount(len(items))
         index = 0
         for item in items:
             illust_id = QTableWidgetItem()
             illust_id.setTextAlignment(Qt.AlignCenter)
             illust_id.setData(Qt.EditRole, QVariant(int(item['illustId'])))
+            illust_id.setBackground(QBrush(QColor('#CCFFFF')))
             self.table_viewer.setItem(index, 0, illust_id)
 
             self.table_viewer.setItem(index, 1, QTableWidgetItem(item['illustTitle']))
@@ -193,36 +351,47 @@ class MainWidget(QWidget):
             user_id = QTableWidgetItem()
             user_id.setTextAlignment(Qt.AlignCenter)
             user_id.setData(Qt.EditRole, QVariant(int(item['userId'])))
+            user_id.setBackground(QBrush(QColor('#CCFFFF')))
             self.table_viewer.setItem(index, 2, user_id)
 
             self.table_viewer.setItem(index, 3, QTableWidgetItem(item['userName']))
-            self.table_viewer.setItem(index, 4, QTableWidgetItem(item['createDate']))
 
             page_count = QTableWidgetItem()
             page_count.setTextAlignment(Qt.AlignCenter)
             page_count.setData(Qt.EditRole, QVariant(item['pageCount']))
-            self.table_viewer.setItem(index, 5, page_count)
+            page_count.setBackground(QBrush(QColor('#CCFFFF')))
+            self.table_viewer.setItem(index, 4, page_count)
+
+            self.table_viewer.setItem(index, 5, QTableWidgetItem(item['createDate']))
             index += 1
+        self.table_viewer.setSortingEnabled(True)
 
     def fetch_info(self):
-        self._set_disabled(True)
-        self._set_disabled(False)
+        self.set_disabled(True)
+        self.set_disabled(False)
 
     def fetch_new(self):
-        self._set_disabled(True)
-        num = int(self.ledit_num.text())
-        new_set = pixiv.get_new(self.glovar.session, self.glovar.proxy, num)
-        updater = []
-        results = []
-        for pid in new_set:
-            fet_pic = pixiv.fetcher(pid)
-            if not fet_pic:
-                print('Not in database.')
-                fet_pic = pixiv.get_detail(self.glovar.session, pid, self.glovar.proxy)
-                updater.append(fet_pic)
-            else:
-                print('Fetch from database')
-            results.append(fet_pic)
-        pixiv.pusher(updater)
-        self._set_disabled(False)
-        self.tabulate(results)
+        """When fetch info button clicked, call get_detail function in pixiv.py."""
+        self.set_disabled(True)
+        pid = self.ledit_pid.text()
+        uid = self.ledit_uid.text()
+        num = int(self.ledit_num.text()) if self.ledit_num.text() else 0
+        # 插入QThread
+        # new_set = set(pid) if pid else pixiv.get_new(self.glovar.session, self.glovar.proxy, user_id=uid, num=num)
+        # updater = []
+        # results = []
+        # for pid in new_set:
+        #     fet_pic = pixiv.fetcher(pid)
+        #     if not fet_pic:
+        #         print('Not in database.')
+        #         fet_pic = pixiv.get_detail(self.glovar.session, pid=pid, proxy=self.glovar.proxy)
+        #         updater.append(fet_pic)
+        #     else:
+        #         print('Fetch from database')
+        #     results.append(fet_pic)
+        # pixiv.pusher(updater)
+        self.fetch_thread = FetchThread(self.glovar.session, self.glovar.proxy, pid, uid, num)
+        self.fetch_thread.fetch_success.connect(self.tabulate)
+        self.fetch_thread.except_signal.connect(globj.show_messagebox)
+        self.fetch_thread.finished.connect(partial(self.set_disabled, False))
+        self.fetch_thread.start()
